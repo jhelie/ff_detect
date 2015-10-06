17c8043580932fdc69113af5486cce6982f5cbba
jhelie <jean.helie@gmail.com>
2015-10-06 11:12:36 +0100

    v0.0.3

--- ff_detect.py
+++ ff_detect.py
@@ -17,7 +17,7 @@ parser = argparse.ArgumentParser(prog='ff_detect', usage='', add_help=False, for
 '''
 **********************************************
 v''' + version_nb + '''
-author: Jean Helie (jean.helie@bioch.ox.ac.uk)
+author: Jean Helie (jean.helie@gmail.com)
 git: https://github.com/jhelie/ff_detect
 **********************************************
 	
